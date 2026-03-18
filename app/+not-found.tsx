9ef12f6eaae8a799193c44b8ff29a048bcebc646
Matheus Coelho Germano <ra2457089>
2026-03-18 19:56:28 -0300

    tres telas not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,12 +1,12 @@
-import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Vazia' }} />
       <View style={styles.container}>
-        <Link href="/" style={styles.button}>
+        <Link href="/about" style={styles.button}>
           volte para Home
         </Link>
       </View>
