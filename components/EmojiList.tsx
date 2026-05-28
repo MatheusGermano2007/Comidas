80d1b46d8b88a3319a3e50294b56189ee20c0662
Matheus Coelho Germano <ra2457089>
2026-05-27 21:28:18 -0300

    Final

--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -14,6 +14,7 @@ export default function EmojiList({ onSelect, onCloseModal }: Props) {
     require("../assets/images/emoji3.png"),
     require("../assets/images/emoji4.png"),
     require("../assets/images/emoji5.png"),
+    require("../assets/images/emoji6.png"),
   ]);
 
   return (
